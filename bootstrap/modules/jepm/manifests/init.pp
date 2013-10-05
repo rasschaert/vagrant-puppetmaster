cdcf0f523e9e73fa79333481dfc1a2d81d21d8ea
Kenny Rasschaert <kenny@inuits.eu>
2013-10-05 18:35:03 +0200

    Stripped useless comments

--- bootstrap/modules/jepm/manifests/init.pp
+++ bootstrap/modules/jepm/manifests/init.pp
@@ -2,39 +2,6 @@
 #
 # Just Enough Puppet Master
 #
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { jepm:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
-# === Authors
-#
-# Kenny Rasschaert <kenny@kennyrasschaert.be>
-#
-# === Copyright
-#
-# Copyright 2013 Kenny Rasschaert, unless otherwise noted.
-#
 class jepm {
   include jepm::repo
   include jepm::install
@@ -115,8 +82,5 @@ class jepm::service {
   service { 'puppetmaster':
     enable => true,
     ensure => running,
-    #hasrestart => true,
-    #hasstatus => true,
-    #require => Class["config"],
   }
 }
