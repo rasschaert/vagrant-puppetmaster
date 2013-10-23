ba6810b82991714454d5240ca7725fb955f5dcf6
Kenny Rasschaert <kenny@inuits.eu>
2013-10-23 08:50:42 +0200

    Disabled the firewall

--- bootstrap/manifests/nodes/puppet.pp
+++ bootstrap/manifests/nodes/puppet.pp
@@ -1,4 +1,13 @@
 node 'puppet.vagrant.local' {
+# The firewall is disabled
+# This is a temporary measure until I add and configure the firewall module
+# to this project. I need to open up 8140 TCP for the Puppet web server,
+# 8081 TCP for the PuppetDB secure PostgreSQL and 53 UDP & TCP for DNS.
+  service { "iptables":
+    enable => false,
+    ensure => stopped,
+  }
+
   include puppet::master
   include puppet::run
 
