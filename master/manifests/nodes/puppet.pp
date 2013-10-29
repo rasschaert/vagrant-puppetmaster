c6ecd52ebc82d0623fe385c7d20045c05caf4f08
Kenny Rasschaert <kenny@inuits.eu>
2013-10-29 09:22:58 +0100

    puppetmaster machine now also uses the correct dns server

--- master/manifests/nodes/puppet.pp
+++ master/manifests/nodes/puppet.pp
@@ -2,8 +2,10 @@ node 'puppet.vagrant.local' {
   include puppetmaster
   include dns::entry
   include dns::server
+  include dns::client
 
   Class['puppetmaster'] ->
   Class['dns::entry'] ->
-  Class['dns::server']
+  Class['dns::server'] ->
+  Class['dns::client']
 }
