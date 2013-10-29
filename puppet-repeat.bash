e4ed516266fd296b9004c8e6b87d87450f5aef92
Kenny Rasschaert <kenny@inuits.eu>
2013-10-29 12:46:20 +0100

    Turns out exit code 2 also means the puppet agent run went fine, who knew?

--- puppet-repeat.bash
+++ puppet-repeat.bash
@@ -2,7 +2,7 @@
 # Do puppet agent runs until a run ends succesfully
 RET=1
 PATH="/usr/bin:/opt/ruby/bin:$PATH" # Possible locations of the puppet binary
-until [[ "$RET" -eq "0" ]]; do
+until [[ "$RET" -eq "0" || "$RET" -eq "2" ]]; do
   puppet agent -t
   RET=$?
 done
